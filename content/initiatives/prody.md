51ee5577edc918928ed46c8d4e12b36e373e5b00
Parth Pant <parthpant4@gmail.com>
2020-08-11 12:20:41 +0530

    added react-responsive carousel

--- content/initiatives/prody.md
+++
@@ -1,5 +0,0 @@
----
-title: "Prodyogiki"
----
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac massa tristique metus auctor luctus sed quis ex. Nam condimentum metus ornare feugiat convallis. Phasellus pharetra accumsan neque, at accumsan mauris ultrices sit amet. Nullam egestas nunc et mi hendrerit, id fermentum metus viverra. Quisque efficitur enim eget ex commodo venenatis.
--- src/components/initiatives.js
+++ src/components/initiatives.js
@@ -3,62 +3,22 @@ import styled from 'styled-components'
 import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
-
-import {ReactComponent as LeftArrow} from "../images/left-arrow.svg"
-import {ReactComponent as RightArrow} from "../images/right-arrow.svg"
-
-
-const SliderDiv =  styled.div`
-    width: 90%;
-    margin: 10px auto;
-    overflow: hidden;
-    .Carousel{
-        /* border: 2px solid #ccc; */
-        height: 400px;
-        position: relative;
-
-        .controls .arrow{
-            position: absolute;
-            top: 50%;
-            cursor: pointer;
-        }
-        .arrow.left{
-            left: 10px;
-        }
-        .arrow.right{
-            right: 10px;
-        }
-    }
-`;
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const Slider = styled.div`
-    height: 100%;
-    display: flex;
-    width: ${({numSlides, slide})=> (numSlides)*100+'%'};
-    transform: ${({numSlides, slide})=> 'translateX(-'+slide*(100/numSlides)+'%)'};
-    transition: transform 0.5s ease-out 0.5s;
-    section{
-        color: white;
-        flex-basis: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        .Container{
-            padding: 1rem 3rem;
-        }
-    }
-
     @media screen and (max-width: 800px){
         section{
             margin-top: 3rem;
+
+            .Container{
+                padding: 1rem 0;
+            }
         }
     }
 `;
 
 export default function Initiatives(){
-    const [slide, setSlide] = useState(0)
-    const [auto, setAuto] = useState(true)
     const data = useStaticQuery(
     graphql`
         query{
@@ -66,8 +26,8 @@ export default function Initiatives(){
             edges {
                 node {
                 childImageSharp {
-                    fixed {
-                        ...GatsbyImageSharpFixed
+                    fluid(maxHeight: 200){
+                        ...GatsbyImageSharpFluid_withWebp
                     }
                 }
                 }
@@ -76,47 +36,13 @@ export default function Initiatives(){
         } 
     `
     )
-
-    const numSlides = data.allFile.edges.length
-
-    if (auto)
-        setTimeout(()=>{autoSlide()},5000);
-
-    function autoSlide(){
-        if(auto){
-            (slide+1 >= numSlides) ? setSlide(0) : setSlide(slide+1)
-        }
-    }
-    function nextSlide(e){
-        if(auto){
-            setAuto(!auto)
-        }
-        (slide+1 >= numSlides) ? setSlide(0) : setSlide(slide+1)
-    }
-    function prevSlide(e){
-        if(auto){
-            setAuto(!auto)
-        }
-        (slide-1 < 0) ? setSlide(numSlides-1) : setSlide(slide-1)
-    }
-
     return (
-        <SliderDiv>
-        <div className='Carousel'>
-            <Slider className='Slider' numSlides={numSlides} slide={slide}>
-                {data.allFile.edges.map(({node})=>(
-                    <section>
-                        <div className='Container'>
-                            <Img fixed={node.childImageSharp.fixed}/>
-                        </div>
-                    </section>
-                ))}
-            </Slider>
-            <div className='controls'>
-                <span className='arrow left' onClick={(prevSlide)}><LeftArrow fill='white' height='20px' width='20px'/></span>
-                <span className='arrow right' onClick={nextSlide}><RightArrow fill='white' height='20px' width='20px'/></span>
-            </div>
-        </div>
-        </SliderDiv>
+        <Carousel autoPlay>
+            {data.allFile.edges.map(({node})=>(
+                <div>
+                    <Img fluid={node.childImageSharp.fluid}/>
+                </div>
+            ))}
+        </Carousel>
     )   
 }
