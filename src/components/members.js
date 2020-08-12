fd605aa4388bc071d28df9e67866639679e83c04
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-08-12 15:11:23 +0530

    Modify query to fetch data separately

--- src/components/members.js
+++ src/components/members.js
@@ -1,29 +1,93 @@
 import React from "react"
-import { graphql,useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
+import { node } from "prop-types"
 
-export default function Members(){
-    const data = useStaticQuery(graphql`
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export default function Members() {
+  const data = useStaticQuery(graphql`
     query {
-        allMembersXlsxSheet1 {
-          edges {
-            node {
-              name
-              year
-              description
-            }
+      allMembersXlsxSheet1 {
+        edges {
+          node {
+            name
+            year
+            description
           }
         }
       }
-        
-    `
-)
+      allMembersXlsxSheet2 {
+        edges {
+          node {
+            name
+            year
+            description
+          }
+        }
+      }
+      allMembersXlsxSheet3 {
+        edges {
+          node {
+            name
+            year
+            description
+          }
+        }
+      }
+      allMembersXlsxSheet4 {
+        edges {
+          node {
+            name
+            year
+            description
+          }
+        }
+      }
+    }
+  `)
 
-    return data.allMembersXlsxSheet1.edges.map( (row,i) => (
+  return (
     <div>
-        <div>{ row.node.name}</div>
-        <div>{ row.node.year}</div>
-        <div>{ row.node.description}</div>
+      <div>
+        {data.allMembersXlsxSheet1.edges.map((row, i) => (
+          <Card>
+            <div>{row.node.name}</div>
+            <div>{row.node.description}</div>
+            <div>{row.node.year}</div>
+          </Card>
+        ))}
+      </div>
+      <div>
+        {data.allMembersXlsxSheet2.edges.map((row, i) => (
+          <Card>
+            <div>{row.node.name}</div>
+            <div>{row.node.description}</div>
+            <div>{row.node.year}</div>
+        </Card>
+        ))}
+      </div>
+      <div>
+        {data.allMembersXlsxSheet3.edges.map((row, i) => (
+          <Card>
+            <div>{row.node.name}</div>
+            <div>{row.node.description}</div>
+            <div>{row.node.year}</div>
+        </Card>
+        ))}
+      </div>
+      <div>
+        {data.allMembersXlsxSheet4.edges.map((row, i) => (
+          <Card>
+            <div>{row.node.name}</div>
+            <div>{row.node.description}</div>
+            <div>{row.node.year}</div>
+        </Card>
+        ))}
+      </div>
     </div>
-    ))
+  )
 }
-
--- src/pages/members.js
+++ src/pages/members.js
@@ -1,15 +1,19 @@
 import React from "react"
-
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Members from "../components/members"
 
+const MainDiv = styled.div`
+    display: flex;
+    flex-direction: column
+`
+
 export default function Member(){
     return(
         <Layout>
             <SEO title="Members"/>
-                <div>Hello</div>
                 <Members></Members>
         </Layout>
     )
