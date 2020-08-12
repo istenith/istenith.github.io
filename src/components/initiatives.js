34aa8d1ec0bed8db01ede8573d289425aa95dbcd
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-08-12 11:49:43 +0530

    Remove unused variables

--- src/components/initiatives.js
+++ src/components/initiatives.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react"
-import styled from 'styled-components'
+import React from "react"
+// import styled from 'styled-components'
 import { useStaticQuery, graphql } from "gatsby"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import Img from "gatsby-image"
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-const Slider = styled.div`
-    @media screen and (max-width: 800px){
-        section{
-            margin-top: 3rem;
+// const Slider = styled.div`
+//     @media screen and (max-width: 800px){
+//         section{
+//             margin-top: 3rem;
 
-            .Container{
-                padding: 1rem 0;
-            }
-        }
-    }
-`;
+//             .Container{
+//                 padding: 1rem 0;
+//             }
+//         }
+//     }
+// `;
 
 export default function Initiatives(){
     const data = useStaticQuery(
