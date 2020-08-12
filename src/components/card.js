591cee845ae5bb24196e98782a96dbd4ce291b09
Parth Pant <parthpant4@gmail.com>
2020-08-12 12:56:55 +0530

    added card component

---
+++ src/components/card.js
@@ -0,0 +1,103 @@
+import { Link } from "gatsby"
+import React, { useState } from "react"
+import Image from "gatsby-image"
+import styled from "styled-components"
+import kebabCase from "lodash/kebabCase"
+
+const BannerImage = styled(Image)`
+    width: 100%;
+    height: 250px;
+
+    img{
+      margin: 0;
+    }
+    @media only screen and (max-width: 786px){
+        width: 100%;
+        height: 200px;
+    }
+`
+
+const CardDiv = styled.div`
+    display: flex;
+    width: 30%;
+    overflow: hidden;
+    margin: 0 1% 2%;
+    background-color: #2a2e35;
+    border-radius: 10px;
+    box-shadow: 0 20px 20px rgba(10,10,10,0.6);
+    transition: 0.2s ease-out;
+
+    flex: 0 0 300px;
+
+    .text{
+      padding: 0 1rem 1rem;
+    }
+    .header{
+        color:white;
+        margin-top: 15px;
+        .title{
+            font-size: xx-large;
+            font-weight: 400;
+        }
+    }
+
+    .tags{
+      color:white;
+      display: flex;
+      .tag{
+          background-image:linear-gradient(0deg,#ff8a00,#e52e71);
+          border-radius: 20px;
+          margin: 5px;
+          padding: 0 4px;
+          min-width: 50px;
+          text-align: center;
+      }
+    } 
+
+    .excerpt{
+        color:white;
+        margin-top: 15px;
+        text-align: justify;
+        max-height: 150px;
+    }
+
+    &:hover{
+      transform: translateY(-2%);
+      transition: 0.2s ease-out;
+    }
+
+    @media only screen and (max-width: 786px){
+        width: 90%;
+        margin: 0 5% 5%;
+    } 
+
+`
+
+const Card = ({title, date, excerpt, slug, fluid, tags}) =>{
+    return (
+    <CardDiv>
+      <Link to={slug} style={{ textDecoration: "none" }}>
+        {fluid && (
+          <BannerImage
+            fluid={fluid}
+            alt="Banner Image"
+          />
+        )}
+        <div className='text'>
+          <div className="header">
+            <div className="title">{title}</div>
+            <div className="date">{date}</div>
+          </div>
+          <div className="excerpt">{excerpt}</div>
+          <div className='tags'>{tags.map((tag) => (
+            <Link to={'/tags/' + kebabCase(tag)} style={{ textDecoration: "none", color: 'white' }}>
+              <div className='tag'>{tag}</div>
+            </Link>
+          ))}</div>
+        </div>
+      </Link>
+    </CardDiv>
+    )
+}
+
+export default Card;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
-import Image from "gatsby-image"
 import styled from "styled-components"
-import kebabCase from "lodash/kebabCase"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Card from "../components/card"
 
 const Search = styled.input`
   background: #e84118;
@@ -38,100 +37,9 @@ const FlexDiv = styled.div`
     flex-wrap: wrap;
     position: relative;
 `
-
-const BannerImage = styled(Image)`
-    width: 100%;
-    height: 250px;
-
-    img{
-      margin: 0;
-    }
-    @media only screen and (max-width: 786px){
-        width: 100%;
-        height: 200px;
-    }
-`
-
-const Card = styled.div`
-    display: flex;
-    width: 30%;
-    overflow: hidden;
-    margin: 0 1% 2%;
-    background-color: #2a2e35;
-    border-radius: 10px;
-    box-shadow: 0 20px 20px rgba(10,10,10,0.6);
-    transition: 0.2s ease-out;
-
-    flex: 0 0 300px;
-
-    .text{
-      padding: 0 1rem 1rem;
-    }
-    .header{
-        color:white;
-        margin-top: 15px;
-        .title{
-            font-size: xx-large;
-            font-weight: 400;
-        }
-    }
-
-    .tags{
-      color:white;
-      display: flex;
-      .tag{
-          background-image:linear-gradient(0deg,#ff8a00,#e52e71);
-          border-radius: 20px;
-          margin: 5px;
-          padding: 0 4px;
-          min-width: 50px;
-          text-align: center;
-      }
-    } 
-
-    .excerpt{
-        color:white;
-        margin-top: 15px;
-        text-align: justify;
-        max-height: 150px;
-    }
-
-    &:hover{
-      transform: translateY(-2%);
-      transition: 0.2s ease-out;
-    }
-
-    @media only screen and (max-width: 786px){
-        width: 90%;
-        margin: 0 5% 5%;
-    } 
-
-`
-
 function Projects({ posts }) {
   return posts.map(({ node }) => (
-    <Card>
-      <Link to={node.fields.slug} style={{ textDecoration: "none" }}>
-        {node.frontmatter.featuredImage && (
-          <BannerImage
-            fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
-            alt="Banner Image"
-          />
-        )}
-        <div className='text'>
-          <div className="header">
-            <div className="title">{node.frontmatter.title}</div>
-            <div className="date">{node.frontmatter.date}</div>
-          </div>
-          <div className="excerpt">{node.excerpt}</div>
-          <div className='tags'>{node.frontmatter.tags.map((tag) => (
-            <Link to={'/tags/' + kebabCase(tag)} style={{ textDecoration: "none", color: 'white' }}>
-              <div className='tag'>{tag}</div>
-            </Link>
-          ))}</div>
-        </div>
-      </Link>
-    </Card>
+    <Card title={node.frontmatter.title} date={node.frontmatter.date} excerpt={node.excerpt} fluid={node.frontmatter.featuredImage.childImageSharp.fluid} slug={node.fields.slug} tags={node.frontmatter.tags}/>
   ))
 }
 
