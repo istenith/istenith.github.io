7a1695bafc04af66212d44a25c8576d3fb7cf660
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-08-12 12:22:56 +0530

    Add and config gatsby-transformer-excel plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,5 +61,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/content/`,
+      },
+    },
+    `gatsby-transformer-excel`,
   ],
 }
