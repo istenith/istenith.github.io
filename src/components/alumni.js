b2158ef0efc379838e287893ed8100bf3049988b
Vasundhra <56582359+xinarou@users.noreply.github.com>
2020-08-19 21:36:09 +0530

    Update alumni.js

--- src/components/alumni.js
+++ src/components/alumni.js
@@ -2,20 +2,16 @@ import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Image from "gatsby-image"
 import styled from "styled-components"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-
-    faFacebook,
-    faInstagram,
-    faLinkedin
-  } from "@fortawesome/free-brands-svg-icons";
 
 const Img = styled(Image)`
-height:100%;
-width:80%;
- 
+  width: 100%;
+  height: 50%;
+
+  img {
+    margin: 0;
+  }
   @media only screen and (max-width: 786px) {
-    width: 70%;
+    width: 100%;
   }
 `
 
@@ -29,100 +25,51 @@ const CardContainer = styled.div`
   flex-wrap: wrap;
   justify-content: space-around;
 
-.Card{
-    position:relative;
-    margin:20px 8px 20px 20px;
-
-    ul li{
-      list-style: none;
-    }
-     
-.inner-box{    
-  position:relative;
-  padding:40px 40px 60px 0px;
-    
-   :before{
-    position:absolute;
-    content:'';
-    right:0px;
-    top:0px;
-    left:70px;
-    bottom:0px;
-    border-radius:10px;
-    background-color:#f5f5f5;
-    transition:all 600ms ease;
-  }
-    ::after{
-    position:absolute;
-    content:'';
-    top:0px;
-    left:70px;
-    bottom:0px;
-    width:0%;
-    border-radius:10px;
-    transition:all 600ms ease;
-    background-image: linear-gradient(0deg, #ff8a00, #e52e71);/* the transition color*/
-  }
-
-    :hover::after{
-    width:78.5%;
-  }
-
-    .social-icons{
-    position:absolute;
-    right:25px;
-    top:50px;
-    z-index:1;
-      
-      li{
-      margin-bottom:20px;
-        a{
-        color:#c00b29;
-        transition: all 0.4s;
-      }
-    }
-  
-  }
-    
-    :hover .content .name,:hover .social-icons li a,:hover .content .position,:hover .content .batch{
-    color:#ffffff;
-  }
-
-    .image{
-    position:relative;
-    z-index:1;
-    weight :90%;
-    border-radius:50%;
-    display:inline-block;
-  }
+  .Card{
+    color: white;
+    max-width: 300px;
+    height: 400px;
+    border-radius: 20px;
+    text-align: left;
+    flex: 0 0 300px;
+    overflow: hidden;
+    background-color: #2a2e35;
+    box-shadow: -16px 0 36px #000;
+    position: relative;
+    /* transition: transform 0.5s ease-out; */
+    /* transition: 0.4s ease-out; */
+    left: 0px;
+    margin: 0.5rem 1rem 1rem;
 
     .content{
-    position:relative;
-    text-align:left;
-    z-index:1;
-    padding:30px 15px 0px 110px;
-       
-    .name{
-      color:#c70919;
-      font-size:18px;
-      font-weight:700;
-      line-height:1.3em;
+        margin-top: 10px;
+        padding: 1rem;
+
+        .name{
+            font-size: x-large;
+            font-family: Montserrat;
+            font-weight: 600;
+        }
+
+        .batch{
+            margin-top: 1rem;
+            display: flex;
+            font-size: medium;
+            font-weight: century-gothic,Montserrat;
+        }
+
+        .position{
+            margin-top: 1rem;
+            font-size: large;
+            font-family: Montserrat;
+            font-weight: 400;
+        }
     }
-    .position{
-      margin-top:5px;
-      color:#393939;
-      font-size:14px;
-    }
-    .batch{
-        margin-top:5px;
-      }
-      
-}
-}
-}
-.
-   @media only screen and (max-width: 680px){
-    width: 75%;
+  }
+
+  @media only screen and (max-width: 680px){
+
+    width: 100%;
     justify-content: center;
     
       .Card{
@@ -161,21 +108,18 @@ export default function Alumni() {
       <CardContainer>
         {data.allAlumniXlsxSheet1.edges.map((row, i) => (
           <div className="Card">
-          <div class="inner-box " data-wow-delay="0ms" data-wow-duration="1500ms">
-                        <ul class="social-icons">
-                            <li><a href="#"><FontAwesomeIcon icon={faFacebook} size="1x" /></a></li>
-                            <li><a href="#"><FontAwesomeIcon icon={faLinkedin} size="1x" /></a></li>
-
-                            <li><a href="#"><FontAwesomeIcon icon={faInstagram} size="1x" /></a></li>
-                        </ul>
-            <Img className="image" fluid={row.node.img.childImageSharp.fluid}></Img>        
+            <Link
+              to={row.node.link}
+              style={{ textDecoration: "none", color: "white" }}
+              target="blank"
+            >
+              <Img fluid={row.node.img.childImageSharp.fluid}></Img>
               <div className="content">
                 <div className="name">{row.node.name}</div>
-                <div className="position">{row.node.position}</div>
                 <div className="batch">{row.node.start} - {row.node.end}</div>
-                
+                <div className="position">{row.node.position}</div>
               </div>
-            </div>
+            </Link>
           </div>
         ))}
       </CardContainer>
