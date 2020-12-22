379b0164b219d908fc3b676ae83bd1d5100a9068
Parth Pant <parthpant4@gmail.com>
2020-12-22 18:46:32 +0530

    blog and poster

--- src/templates/elements.js
+++ src/templates/elements.js
@@ -29,6 +29,11 @@ export const Pre = styled.pre`
     background-color: #253270;
     border-radius: 10px;
     padding: 10px;
+    white-space: pre-wrap;       /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+    white-space: -pre-wrap;      /* Opera 4-6 */
+    white-space: -o-pre-wrap;    /* Opera 7 */
+    word-wrap: break-word;       /* Internet Explorer 5.5+ */
 `;
 
 export const Code = styled.code`
