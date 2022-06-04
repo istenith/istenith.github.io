e1a1aa321b8ef31496a5953241f83456bf593745
lucifer1708 <20bce091@nith.ac.in>
2022-06-04 11:20:04 +0530

    added files

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,88 +1,146 @@
-import React from "react"
-import styled from 'styled-components'
-// import { Link } from "gatsby"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react'
+// import '../styles/footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-    faYoutube,
-    faFacebook,
-    faTwitter,
-    faInstagram,
-    faGithub,
-    faLinkedin
-  } from "@fortawesome/free-brands-svg-icons";
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import { faEnvelope, faMap } from '@fortawesome/free-regular-svg-icons'
 
-const Container = styled.footer`
-    min-height: 60px;
-    display: flex;
-    justify-content: space-around;
-    background-color: #242729;
+const Footer = styled.div`
+.container{
+  background-color: #101010;
+  padding-top: 2rem;
+  .contact-row{
+    margin: 0rem 4rem 0rem 4rem;
+    display:flex;
+    justify-content:space-between;
     align-items:center;
+    font-size: 1rem;
+    a{
+      font-weight: medium;
+      padding: 2px;
+    }
+    a:hover{
+      /* font-weight: bold; */
+      color: rgba(108, 50, 224, 0.8);
+      transition: 0.2s ease-in-out;
+    }s
+  }
+  a{
+    text-decoration: none;
     color: white;
-    /* box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.75); */
-
-    .Copyright{
-        flex: 1;
-        text-align: center;
+  
+  }
+  .social-icons{
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0.25rem;
+    li{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.5rem;
+      margin: 0.25rem;
+      FontAwesomeIcon{
+        color: black;
+      }
+      .main-icon{
+        font-size: 28px;
+      }
+      .main-icon:hover{
+        transition: 0.25s ease-in-out;
+        color: rgba(108, 50, 224, 0.99);
+      }
     }
 
-    .icons{
-        flex: 1;
-        display: flex;
-        flex-wrap:wrap;
-        a{
-            margin: 10px;
-            color: white;
-        }
-    }
-    
-    .text{
-        flex:2;
-        margin:0;
-        width: 20%;
-        font-size: 0.8rem;
-        margin: 0 10px;
-        text-align: center;
-    }
-
-    @media only screen and (max-width: 680px){
-        .text{
-            font-size: 0.5rem;
-        }
-
-        .Copyright{
-            font-size: 0.7rem;
-        }
-    }
-`;
-
-const Footer = ()=>{
-    return(
-        <Container>
-            <div className='Copyright'>© {new Date().getFullYear()}, ISTENITH</div>
-            <p className='text'>Wanna talk to ISTE? Or be a part of it? You can contact any of our members personally or 📧 us at <strong><a href="mailto:teamiste@gmail.com">teamiste@gmail.com</a></strong>!</p>
-            <div className='icons'>
-                <a href="https://www.facebook.com/teamistenith/">
-                    <FontAwesomeIcon icon={faFacebook} size="1x" />
-                </a>
-                <a href="https://twitter.com/istenith">
-                    <FontAwesomeIcon icon={faTwitter} size="1x" />
-                </a>
-                <a href="https://www.instagram.com/teamistenith/?hl=en">
-                    <FontAwesomeIcon icon={faInstagram} size="1x" />
-                </a>
-                <a href="https://www.youtube.com/channel/UCIC-jhIbxXkcDxyQtJ3OKkQ">
-                    <FontAwesomeIcon icon={faYoutube} size="1x" />
-                </a>
-                <a href="https://www.linkedin.com/company/iste-nith/?originalSubdomain=in">
-                    <FontAwesomeIcon icon={faLinkedin} size="1x" />
-                </a>
-                <a href="https://github.com/istenith">
-                    <FontAwesomeIcon icon={faGithub} size="1x" />
-                </a>
-            </div> 
-        </Container>
-    )
+  }
+  .social-icons:hover{
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+    background-color: #161516;
+    border-radius: 10px;
+  }
+  .copyright{
+    background-color: #161516;
+    padding: 1rem;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: medium;
+  }
 }
 
+`
+const footer = () => {
+  return (
+    <Footer>
+      <footer className='site-footer'>
+        <div className="container">
+          <div className="contact-row">
+            <a href='https://www.google.com/maps/place/National+Institute+of+Technology,+Hamirpur+(NIT+-+Hamirpur)/@31.7084291,76.5273526,17z/data=!3m1!4b1!4m5!3m4!1s0x3904d5487e12c4a1:0x395f92d3a202a7d0!8m2!3d31.7084291!4d76.5273526'><div className="location"><FontAwesomeIcon icon={faMap} size='1x' /> National Institute of Technology, Hamirpur</div></a>
+            <a href='mailto:teamiste@gmail.com'><div className="email"><FontAwesomeIcon icon={faEnvelope} size='1x' /> teamiste@gmail.com</div></a>
+          </div>
+          <div className="social-icons">
+            <li>
+              <Link
+                className='main-icon'
+                to='https://www.facebook.com/teamistenith/'
+              >
+                <FontAwesomeIcon icon={faFacebook} size='1x' />
+              </Link>
+            </li>
+            <li>
+              <Link className='main-icon' to='https://twitter.com/istenith'>
+                <FontAwesomeIcon icon={faTwitter} size='1x' />
+              </Link>
+            </li>
+            <li>
+              <Link
+                className='main-icon'
+                to='https://github.com/istenith'
+              >
+                <FontAwesomeIcon icon={faGithub} size='1x' />
+              </Link>
+            </li>
+            <li>
+              <Link
+                className='main-icon'
+                to='https://www.instagram.com/teamistenith/?hl=en'
+              >
+                <FontAwesomeIcon icon={faInstagram} size='1x' />
+              </Link>
+            </li>
+            <li>
+              <Link className='main-icon' to='#'>
+                <FontAwesomeIcon icon={faLinkedin} size='1x' />
+              </Link>
+            </li>
+            <li>
+              <Link
+                className='main-icon'
+                to='https://www.youtube.com/channel/UCIC-jhIbxXkcDxyQtJ3OKkQ'
+              >
+                <FontAwesomeIcon icon={faYoutube} size='1x' />
+              </Link>
+            </li>
+          </div>
+          <div className="copyright">
+            © {new Date().getFullYear()}, ISTE-NITH{' '}
+          </div>
+        </div>
+      </footer>
+    </Footer>
+  )
+}
 
-export default Footer
+export default footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,191 +1,273 @@
-import { Link } from "gatsby"
-import React, { useState } from "react"
-import styled from "styled-components"
-// import Gradient from "rgt"
-const Nav = styled.nav`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  min-height: 8vh;
-  box-sizing: border-box;
-  color: white;
-  background-color: #2a2e35;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.38);
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  margin-bottom: 20px;
-  z-index: 2000;
-  .logo {
-    z-index: 400;
-    letter-spacing: 5px;
-    margin: 5px 0 0;
-    padding: 0;
-    Link {
-      color: inherit;
-      text-decoration: none;
-    }
-  }
+import React, { useState } from 'react'
+import '../styles/header.css'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import Hamburger from 'hamburger-react'
+import Fade from 'react-reveal/Fade'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import ISTE_Logo from '../images/iste_logo_hollow.svg'
+import Bounce from 'react-reveal/Bounce'
 
-  @media only screen and (max-width: 680px) {
-    .logo {
-      margin-left: 20%;
-    }
-  }
-`
-
-const Ul = styled.ul`
-  display: flex;
-  width: 30%;
-  margin: 0;
-  justify-content: space-around;
-  z-index: 0;
-
-  li {
-    list-style: none;
-    letter-spacing: 2px;
-    margin: 0;
-    padding-right: 4px;
-    padding-left: 4px;
-    transition: 2ms;
-  }
-
-  @media screen and (max-width: 1200px) {
-    width: 45%;
-  }
-
-  @media screen and (max-width: 800px) {
-    width: 60%;
-  }
-
-  @media screen and (max-width: 790px) {
-    position: fixed;
-    flex-flow: column nowrap;
-    align-items: center;
-    background-color: #2a2e35;
-    opacity: 1;
-    top: 8vh;
-    height: 92vh;
-    width: 100%;
-    transform: ${({ open }) => (open ? "translateY(0%)" : "translateY(-150%)")};
-    transition: transform 0.5s ease-in-out;
-    li {
-      opacity: ${({ open }) => (open ? 1 : 0)};
-      transition: opacity 1s ease-in-out;
-    }
-  }
-`
-
-const Burger = styled.div`
-  display: none;
-  flex-direction: column;
-  div {
-    width: 23px;
-    height: 3px;
-    background-color: white;
-    margin: 4px;
-    transform-origin: 1px;
-    transition: all 0.3s linear;
-    &:nth-child(1) {
-      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
-    }
-    &:nth-child(2) {
-      transform: ${({ open }) => (open ? "translateX(100%)" : "translateX(0)")};
-      opacity: ${({ open }) => (open ? 0 : 1)};
-    }
-    &:nth-child(3) {
-      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
-    }
-  }
-  @media screen and (max-width: 790px) {
-    display: block;
-  }
-`
-const convention = styled(Link)`
-  text-decoration: none;
-  font-weight: bold;
-  border: 2px solid black;
-  transition: 0.2s;
-  &:hover {
-    color: #e63468;
-  }
-`
 const StyledLink = styled(Link)`
+  width: 80%;
+  font-size: 1.8vw;
   text-decoration: none;
   color: white;
+  padding: 1.8%;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.05);
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+  border-radius: 10px 10px 0px 0px;
+  /* border: 1px solid white; */
+  /* border:1px solid white; */
+  @media screen and (max-width:1356px){
+    font-size:40px;
+  }
+  &::before {
+    /* position: absolute;
+    content: ' ';
+    z-index: -1;
+    width: 25%;
+    top: 50%;
+    left: -15%;
+    height: 1px;
+    /* background:#161516; */
+    /* background: #E05215; */
+    background: rgba(108, 50, 224, 0.3);
+    transition: all 0.3s ease; */
+  }
 
+  &:hover:before {
+    left: -10%;
+  }
   transition: 0.2s;
   &:hover {
-    color: #e63468;
+    /* background: #E0521550; */
+    /* background:#161516; */
+    background: rgba(22, 21, 22, 0.8);
+    border-bottom: 0.5px solid rgba(108, 50, 224, 0.6);
+    color: #fff;
+  }
+`
+const Navmenu = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+`
+const Menu = styled.div`
+  font-size: 1vw;
+  width: 40vw;
+  height: 100vh;
+  left: 0;
+  z-index: 99;
+  /* border: 2px solid white; */
+  background-color: rgba(22, 21, 22, 0.7);
+  backdrop-filter: blur(10px);
+  /* box-shadow: 0 30px 40px rgba(0,0,0,1); */
+  @media screen and (max-width: 700px) {
+    width: 100vw;
+  }
+  ul {
+    text-align: center;
+    padding-top: 25vh;
+  }
+`
+const Menuburger = styled.div`
+  position: absolute;
+  right: 0;
+  @media screen and (min-width: 699px) {
+    display: none;
   }
 `
 
-export default function Header() {
-  const [open, setOpen] = useState(false)
+const SocialLinks = styled.div`
+  display: flex;
+  position: absolute;
+  left: 50%;
+  width: 100%;
+  bottom: 0;
+  transform: translate(-50%, 0);
+  div {
+    flex: none;
+    margin: auto 1%;
+    svg {
+      width: 2.5vw;
+      height: 2.5vw;
+      @media screen and (max-width: 1356px) {
+        width: 43px;
+        height: 43px;
+      }
+    }
+  }
+`
+const Img = styled.img`
+  margin: 4%;
+  width: 100px;
+  position: absolute;
+`
+
+const ImgLogo = styled.img`
+  margin: 2%;
+  z-index: 100;
+  width: 100px;
+  left: 0;
+  top: 0;
+  position: fixed;
+`
+
+const ExtraForClose = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+`
+
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false)
+
+  function clicked() {
+    setOpen(!isOpen)
+  }
+
+  const ToggleNav = () => {
+    return (
+      <>
+        <ExtraForClose onClick={clicked}></ExtraForClose>
+        <Menu>
+          <Fade bottom delay={900}>
+            <Menuburger>
+              <Hamburger
+                toggled={isOpen}
+                toggle={setOpen}
+                color={'white'}
+                direction={'right'}
+              />
+            </Menuburger>
+            <div>
+              <Img src={ISTE_Logo} />
+            </div>
+          </Fade>
+          <ul>
+            <Bounce right delay={100}>
+              <StyledLink
+                to='/'
+                activeClassName='active'
+                activeStyle={{
+                  fontWeight: 'bold',
+                  color: 'rgba(108, 50, 224, 0.7)',
+                }}
+              >
+                Home
+              </StyledLink>
+            </Bounce>
+            <Bounce right delay={120}>
+              <StyledLink
+                to='/projects'
+                activeClassName='active'
+                activeStyle={{
+                  fontWeight: 'bold',
+                  color: 'rgba(108, 50, 224, 0.7)',
+                }}
+              >
+                Blog
+              </StyledLink>
+            </Bounce>
+            <Bounce right delay={140}>
+              <StyledLink
+                to='/events'
+                activeClassName='active'
+                activeStyle={{
+                  fontWeight: 'bold',
+                  color: 'rgba(108, 50, 224, 0.7)',
+                }}
+              >
+                Events
+              </StyledLink>
+            </Bounce>
+            {/* <Bounce right delay={160}>
+              <StyledLink
+                to='/activites'
+                activeClassName='active'
+                activeStyle={{ fontWeight: 'bold', color: '#E05215' }}
+              >
+                Activites
+              </StyledLink>
+            </Bounce> */}
+            <Bounce right delay={180}>
+              <StyledLink
+                to='/members'
+                activeClassName='active'
+                activeStyle={{
+                  fontWeight: 'bold',
+                  color: 'rgba(108, 50, 224, 0.7)',
+                }}
+              >
+                Members
+              </StyledLink>
+            </Bounce>
+            <Bounce right delay={220}>
+              <StyledLink
+                to='/#about'
+                activeClassName='active'
+                activeStyle={{
+                  fontWeight: 'bold',
+                  color: 'rgba(108, 50, 224, 0.7)',
+                }}
+              >
+                About
+              </StyledLink>
+            </Bounce>
+          </ul>
+          <SocialLinks>
+            <div>
+              <Fade left delay={1400}>
+                <a href='https://www.instagram.com/teamistenith/?hl=en'>
+                  <InstagramIcon style={{ color: 'white' }} />
+                </a>
+              </Fade>
+            </div>
+            <div>
+              <Fade left delay={1200}>
+                <a href='https://www.facebook.com/teamistenith/'>
+                  <FacebookIcon style={{ color: 'white' }} />
+                </a>
+              </Fade>
+            </div>
+            <div>
+              <Fade left delay={1000}>
+                <a href='https://www.linkedin.com/company/iste-nith'>
+                  <LinkedInIcon style={{ color: 'white' }} />
+                </a>
+              </Fade>
+            </div>
+          </SocialLinks>
+        </Menu>
+      </>
+    )
+  }
+
   return (
-    <Nav>
-      <h2 className="logo">
-        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-          ISTE-NITH
+    <div className='Navbar' style={{ zIndex: '+1000' }}>
+      <span className='nav-logo'>
+        <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>
+          {!isOpen ? <ImgLogo src={ISTE_Logo} /> : <></>}
         </Link>
-      </h2>
-      <Ul open={open}>
-        <li>
-          <StyledLink
-            to="/projects/"
-            activeStyle={{ fontWeight: "bold", color: "#e63468" }}
-          >
-            Blog
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink
-            to="/events/"
-            activeStyle={{ fontWeight: "bold", color: "#e63468" }}
-          >
-            Events
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink
-            to="/members/"
-            activeStyle={{ fontWeight: "bold", color: "#e63468" }}
-          >
-            Members
-          </StyledLink>
-        </li>
-
-        {/* <li>
-          <StyledLink
-            href="https://www.istenith.com/convention/"
-            target="_blank"
-          >
-            <Gradient dir="left-to-right" from="#00DFFF" to="#e63468">
-            
-              Convention
-            </Gradient>
-          </StyledLink>
-        </li> */}
-
-        <li>
-          <StyledLink
-            to="/#about"
-            activeStyle={{ fontWeight: "bold", color: "#e63468" }}
-          >
-            About Us
-          </StyledLink>
-        </li>
-      </Ul>
-      <Burger
-        onClick={() => {
-          setOpen(!open)
-        }}
-        open={open}
-      >
-        <div />
-        <div />
-        <div />
-      </Burger>
-    </Nav>
+      </span>
+      <Hamburger
+        toggled={isOpen}
+        toggle={setOpen}
+        color={'white'}
+        direction={'right'}
+      />
+      <Navmenu>{isOpen ? <ToggleNav /> : <></>}</Navmenu>
+    </div>
   )
 }
+
+export default Navbar
