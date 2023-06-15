f3e5af397f4119896d1e466947a2ca15d99bd4bd
BhandariG29 <lakshyabhandariktm@gmail.com>
2023-06-15 16:36:09 +0530

    finally

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Img from "gatsby-image"
 import styled from "styled-components"
@@ -9,15 +9,13 @@ import Aos from "aos";
 import Footer from "../components/footer"
 import Button from 'react-bootstrap/Button';
 
-
 const Container = styled.div`
   color: white;
   border-radius: 20px;
-  a{
+  a {
     color:#000!important;
   }
   .links {
-
     .link {
       background-color: #3a3e45;
       text-decoration: none;
@@ -25,7 +23,6 @@ const Container = styled.div`
       border-top-left-radius: 15px;
       border-bottom-right-radius: 15px;
       transition: 0.5s;
-
       &:hover {
         color: #2a2e35;
         background-color: white;
@@ -36,123 +33,115 @@ const Container = styled.div`
       }
     }
   }
-
   .FlexContainer {
     border-radius: 20px;
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-    display:flex;
-    margin-bottom:3vh;
-    @media screen and (max-width:1312px){
-      display:block;
-      margin: 10px, 10px, 10px, 10px;
-      .btn-primary{
+    display: flex;
+    margin-bottom: 3vh;
+    @media screen and (max-width:1312px) {
+      display: block;
+      margin: 10px;
+      .btn-primary {
         margin-left: 10px;
         margin-top: 17px;
         z-index: 10;
-
       }
     }
     .Image {
       background: rgba(108, 50, 224, 0.55);
-      border-radius:20px;
+      border-radius: 20px;
       z-index: 2;
-      flex:none;
-      padding:20px 20px 0 20px;
-      width:15%;
+      flex: none;
+      padding: 20px 20px 0 20px;
+      width: 15%;
       box-shadow: 20px 20px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.6);
-      @media screen and (max-width:1312px){
-        width:100%;
-        margin: 10px, 10px, 10px, 10px;
+      @media screen and (max-width:1312px) {
+        width: 100%;
+        margin: 10px;
       }
+      Img {
+        @media screen and (max-width:1312px) {
+          width: 100%;
+        }
       }
-      Img{
-        @media screen and (max-width:1312px){
-        width:100%;
-        ${"" /* height:200px!important; */}
-      }
-      }
-      }
-      .Title {
-        text-align:center;
-        font-size: 18px;
-        margin-top:20px;
-        color: white;
-        Z-index:99;
-        ${"" /* border: 1px solid red; */}
-      }
+    }
+    .Title {
+      text-align: center;
+      font-size: 18px;
+      margin-top: 20px;
+      color: white;
+      Z-index: 99;
     }
   }
   .EventContent {
     background: rgba(108, 50, 224, 0.45);
     border-radius: 20px;
-    padding:20px;
-    margin-left:-3px;
-    box-shadow: ;
+    padding: 20px;
+    margin-left: -3px;
   }
-  a{
+  a {
     color: white!important;
   }
-  .btn-primary{
+  .btn-primary {
     z-index: 10;
-    margin-left: -168px;
-    margin-top: 196px;
     background-color: rgb(108 50 224 / 45%);
     color: white;
     border-radius: 56px;
     height: 46px;
     width: 134px;
     border-color: azure;
+    position: relative;
+    overflow: hidden;
+
+    &::after, &::before {
+      content: '';
+      background-color: rgb(224 82 21 / 85%);
+      height: 50%;
+      width: 0;
+      position: absolute;
+      transition: 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+      -webkit-transition: 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+      z-index: -1;
+    }
+
+    &:hover::before {
+      width: 100%;
+      right: 0;
+      left: auto;
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      right: auto;
+    }
+
+    &:hover::after {
+      width: 100%;
+      left: 0;
+      right: auto;
+    }
+
+    &:after {
+      bottom: 0;
+      right: 0;
+      left: auto;
+    }
   }
 
-
-
-
-   .btn-primary::after, .btn-primary::before{
-    content: '';
-    background-color: rgb(224 82 21 / 45%);
-    height: 50%;
-    width: 0;
-    position: absolute;
-    transition: 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
-    -webkit-transition: 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
-    z-index: -1;
-  }
-  .btn-primary:hover::before{
-    width: 100%;
-    right: 0;
-    left: auto;
-  }
-  .btn-primary::before{
-    top: 0;
-    left: 0;
-    right: auto;
-  }
-  .btn-primary:hover:after{
-    width: 100%;
-    left: 0;
-    right: auto;
-  }
-  .btn-primary::after{
-    bottom: 0;
-    right: 0;
-    left: auto;
-  }
-
-
-
-
-  .gatsby-image-wrapper{
-    img{
-      ${"" /* @media screen and (max-width:1312px){
-        width:20%!important;
-        height:20%!important;
-      } */}
+  .gatsby-image-wrapper {
+    img {
+      // CSS for gatsby-image
     }
   }
 `;
 
 export default function Events() {
-	const list = useStaticQuery(graphql`
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
+  const list = useStaticQuery(graphql`
     query {
       allMarkdownRemark(
         filter: { fileAbsolutePath: { regex: "/.*initiatives/" } }
@@ -177,36 +166,37 @@ export default function Events() {
     }
   `);
 
-	const data = list.allMarkdownRemark.edges;
-	console.log(data);
-
+  const data = list.allMarkdownRemark.edges;
+  console.log(data);
 
   return (
     <>
-    <Layout>
-      <SEO title="Members" />
-      {data.map(({ node }) => (
-        <Container>
-          {/* <div data-aos="fade-right"> */}
-          <div className="FlexContainer">
-            <div className="Image">
-              <Img
-                fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
-                style={{margin:'0!important'}}
-              />
-              <h1 className="Title">{node.frontmatter.title}</h1>
+      <Layout>
+        <SEO title="Members" />
+        {data.map(({ node }) => (
+          <Container>
+            <div className="FlexContainer">
+              <div className="Image">
+                <Img
+                  fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
+                  style={{ margin: '0!important' }}
+                />
+                <h1 className="Title">{node.frontmatter.title}</h1>
+              </div>
+              <div
+                dangerouslySetInnerHTML={{ __html: node.html }}
+                className="EventContent"
+              ></div>
+              <div>
+                <Button className="btn-primary eventBtn">
+                  <a href="">More info</a>
+                </Button>{' '}
+              </div>
             </div>
-            <div
-              dangerouslySetInnerHTML={{ __html: node.html }}
-              className="EventContent"
-            ></div>
-            <div><Button class="eventBtn"><a href="">More info</a></Button>{' '}</div> 
-          </div>
-        </Container>
-      ))}
-    </Layout>
-    <Footer />
+          </Container>
+        ))}
+      </Layout>
+      <Footer />
     </>
   )
 }
-
