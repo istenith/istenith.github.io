91e121b6afafc985d96b32ef114e9575a8511188
Swastik Sharma <swastkk@gmail.com>
2023-07-03 01:04:19 +0530

    Update setupTest.js for jest-canvas error
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
 require('jest-canvas-mock');
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
