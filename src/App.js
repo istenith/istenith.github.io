09cf852f2938d27fb43085721f790cb11ff039b9
Swastik Sharma <swastkk@gmail.com>
2023-08-09 21:06:06 +0530

    Coming soon added for the timebeing
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import underConstructionImage from './static/images/underconstruction.png';
+
 import { HashRouter } from "react-router-dom";
 
 import Navbar from "./components/navbar";
@@ -17,16 +19,35 @@ const App = () => {
   //   setTheme(currentTheme)
   // }
   return (
-    <>
-      <Preloader />
-      <AnimatedCursor />
-      <HashRouter>
-        <Headroom>
-          <Navbar />
-        </Headroom>
-        <AnimatedRoutes />
-      </HashRouter>
-    </>
+    // Uncomment the below lines to Have everything working!
+    // <>
+    //   <Preloader />
+    //   <AnimatedCursor />
+    //   <HashRouter>
+    //     <Headroom>
+    //       <Navbar />
+    //     </Headroom>
+    //     <AnimatedRoutes />
+    //   </HashRouter>
+    // </>
+    <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '3rem',
+      flexDirection: 'column',
+      fontFamily: 'Anton, sans-serif',
+    }}
+  >
+    <img
+      src={underConstructionImage}
+      alt="Under Construction"
+      style={{ width: '200px', height: 'auto' }} // Adjust the size as needed
+    />
+    <p style={{ marginTop: '1rem', fontFamily: 'Anton, sans-serif' }}>Coming Soon!</p>
+  </div>
   );
 };
 
