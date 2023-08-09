343685e112fb7a5528a051c619cc195678454050
Swastik Sharma <swastkk@gmail.com>
2023-08-09 21:10:18 +0530

    Errors removed for the deployment
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import underConstructionImage from './static/images/underconstruction.png';
 
-import { HashRouter } from "react-router-dom";
-
-import Navbar from "./components/navbar";
-import Headroom from "react-headroom";
-import AnimatedCursor from "./components/animatedCursor";
-import AnimatedRoutes from "./components/AnimatedRoutes";
+// import { HashRouter } from "react-router-dom";
+// import Navbar from "./components/navbar";
+// import Headroom from "react-headroom";
+// import AnimatedCursor from "./components/animatedCursor";
+// import AnimatedRoutes from "./components/AnimatedRoutes";
 // eslint-disable-next-line
-import Preloader from "./components/Preloader";
+// import Preloader from "./components/Preloader";
 // eslint-disable-next-line
 
 const App = () => {
