7e2bc72436c5d709d53afa36cd46667801feb618
Ankur Yadav <ankuryadav@Ankurs-MacBook-Air.local>
2023-08-23 18:29:36 +0530

    Image tags added

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,10 +1,28 @@
 import React, { useState, useEffect, useCallback } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import data from "../data/galleryData";
+import styled from "styled-components";
+
+const ImageWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const Tag = styled.div`
+  position: absolute;
+  bottom: 5px;
+  left: 5px;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  padding: 4px 8px;
+  border-radius: 4px;
+  font-size: 12px;
+`;
 
 const ImageGallery = () => {
   const [model, setModel] = useState(false);
   const [tempimgSrc, setTempImgSrc] = useState("");
+  const [tagContent, setTagContent] = useState(""); // New state for tag content
 
   const closeModel = useCallback(() => {
     setModel(false);
@@ -23,8 +41,9 @@ const ImageGallery = () => {
     };
   }, [closeModel]);
 
-  const getImg = (imgSrc) => {
+  const getImg = (imgSrc, tag) => {
     setTempImgSrc(imgSrc);
+    setTagContent(tag); // Set the tag content for the selected image
     setModel(true);
   };
 
@@ -32,17 +51,18 @@ const ImageGallery = () => {
     <>
       <div className={model ? "model open" : "model"}>
         <img src={tempimgSrc} alt="" />
+        <Tag>{tagContent}</Tag>
         <CloseIcon onClick={closeModel} />
       </div>
       <div className="gallery">
         {data.map((item, index) => (
-          <div
-            className="pics"
+          <ImageWrapper
             key={index}
-            onClick={() => getImg(item.imgSrc)}
+            onClick={() => getImg(item.imgSrc, item.tag)}
           >
             <img src={item.imgSrc} alt="iste-img" style={{ width: "100%" }} />
-          </div>
+            <Tag>{item.tag}</Tag>
+          </ImageWrapper>
         ))}
       </div>
     </>
